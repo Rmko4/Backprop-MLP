fixed the backprop implementation

mlp.py
@@ -5,11 +5,12 @@ import sklearn.datasets
 import matplotlib.pyplot as plt
 
 N_FEATURES = 2
-HIDDEN_UNITS = [20]
+HIDDEN_UNITS = [5]
 N = 500
+RUNS = 2
 
 
-RUN_EAGERLY = True
+RUN_EAGERLY = False
 
 # MLP uses simple forward model so Sequential is subclassed
 
@@ -24,7 +25,7 @@ class MLP(keras.Sequential):
 
         # A dense layer is added for the listed number units in each layer.
         for n in units_config[1:-1]:
-            # NOTE: Sigmoid for some reason does not work, not sure yet why
+            keras.activations.relu
             self.add(keras.layers.Dense(n, activation='relu',
                                         kernel_initializer='glorot_uniform', bias_initializer='zeros'))
         self.add(keras.layers.Dense(units_config[-1], activation='sigmoid',
@@ -33,30 +34,6 @@ class MLP(keras.Sequential):
 
         self.summary()
 
-    # def call(self, inputs, training=False, mask=None):
-    #     u = inputs
-    #     for layer in range(self.n_layers):
-    #         x = self.layers[layer](u)
-    #         a =
-
-    #     return outputs
-    # def calc_gradients(self, deltas, theta, X):
-    #     # For each hidden layer
-    #     for layer in range(self.n_hidden_layers - 1, -1, -1):
-    #         # Find length of current layer
-    #         l_m0 = tf.shape(theta[2*layer])[0]
-    #         delta_m = [0] * l_m0
-    #         # For every hidden unit in the layer
-    #         for i in range(l_m0):
-    #             # Compute sigmoid derivative with activation of i-th unit in current layer
-    #             d_sig = X[layer][0, i] * (1 - X[layer][0, i])
-    #             # Get length of previous layer
-    #             l_m1 = tf.shape(theta[2*(layer - 1)])[0]
-    #             delta_m_i = 0
-    #             for j in range(l_m1):
-    #                 delta_m_i += deltas[-1][0, j] * theta[2*layer][i, j]
-    #             delta_m_i *= d_sig
-    #             delta_m[i] = delta_m_i
 
     # Implementation of the train step function for a batch of data.
     def train_step(self, data):
@@ -83,12 +60,24 @@ class MLP(keras.Sequential):
 
         theta = self.trainable_variables
 
-        def d_dx_MSE(x, y):
-            return 2 * (x - y)
+        def d_dypred_MSE(y_true, y_pred):
+            return 2 * (y_true - y_pred)
+
+        def d_dypred_binary_crossentropy(y_true, y_pred):
+            eps = keras.backend.epsilon()
+            return (1 - y_true) / (1 - y_pred + eps) - (y_true / y_pred + eps) 
+
+        def d_dx_sigmoid(x):
+            return x * (1 - x)
+
+        def d_dx_ReLU(x):
+            return 1. * tf.cast((x > 0), tf.float32)
 
         # Compute deltas for last layer
-        d_dx_mse = d_dx_MSE(X[-1], y)
-        delta_k = d_dx_mse * X[-1] * (1 - X[-1])
+        d_dx_loss = d_dypred_binary_crossentropy(y, X[-1])
+        # if tf.math.is_nan(d_dx_loss)[0]:
+        #     tf.print(d_dx_loss)
+        delta_k = d_dx_loss * d_dx_sigmoid(X[-1])
         deltas.append(delta_k)
 
         # Compute deltas for hidden layers
@@ -98,7 +87,7 @@ class MLP(keras.Sequential):
             W_k_tile = tf.tile(W_k, [batch_size, 1, 1])
 
             # [batch size x L^k]
-            d_sigma = X[layer] * (1 - X[layer])
+            d_sigma = d_dx_ReLU(X[layer])
             dak1_dak = tf.linalg.matvec(W_k_tile, deltas[-1])
             delta_k = d_sigma * dak1_dak
 
@@ -119,6 +108,14 @@ class MLP(keras.Sequential):
             bias_gradient = tf.reduce_mean(bias_gradient, axis=0)
             theta_gradient.append(bias_gradient)
 
+        with tf.GradientTape() as tape:
+            y_pred = self(u, training=True)  # Forward pass of the MLP
+
+            # Compute the loss values
+            # (the loss function is configured in `compile()`)
+            loss = self.compiled_loss(y, y_pred)
+
+        gradients = tape.gradient(loss, theta)
         # Update weights by providing the optimizer (SGD) with the gradient.
         self.optimizer.apply_gradients(zip(theta_gradient, theta))
 
@@ -129,10 +126,10 @@ class MLP(keras.Sequential):
         # Return a dict mapping metric names to current value
         return {m.name: m.result() for m in self.metrics}
 
-    def compile(self, automatic_differentiation=False, learning_rate=0.01, loss_weights=None, weighted_metrics=None,
+    def compile(self, automatic_differentiation=False, learning_rate=0.02, loss_weights=None, weighted_metrics=None,
                 run_eagerly=None, steps_per_execution=None, **kwargs):
         self.automatic_differentiation = automatic_differentiation
-        optimizer = keras.optimizers.SGD(learning_rate=learning_rate, momentum=0) # Without momentum the optimizer is hopelessly bad
+        optimizer = keras.optimizers.SGD(learning_rate=learning_rate)
         # Only MSE is supported due to hard coded back propagation on MSE loss. TODO
         loss = keras.losses.BinaryCrossentropy(from_logits=False) #NOTE: Binary crossentropy might be preferred due to binary class prediction.
         # loss = keras.losses.MeanSquaredError()
@@ -158,23 +155,37 @@ def plot_data(X, Y):
 
 
 def main():
-    u, y = load_data(N)
-    # plot_data(u, y)
+    # plot_data(*load_data(N))
 
-    split_i = int(.8*N)
-    u_train = u[:split_i]
-    y_train = y[:split_i]
-    u_test = u[split_i:]
-    y_test = y[split_i:]
+    accuraccy = []
+    histories = []
+    y_test_s = []
+    y_test_pred_s = []
+    for _ in range(RUNS):
+        u, y = load_data(N)
 
-    mlp = MLP([N_FEATURES, *HIDDEN_UNITS, 1])
-    mlp.compile(automatic_differentiation=True, learning_rate=0.02, run_eagerly=RUN_EAGERLY)
+        split_i = int(.8*N)
+        u_train = u[:split_i]
+        y_train = y[:split_i]
+        u_test = u[split_i:]
+        y_test = y[split_i:]
 
-    mlp.fit(u_train, y_train, batch_size=1, epochs=100, validation_split=0.2)
-    y_test_pred = mlp.predict_class(u_test)
+        mlp = MLP([N_FEATURES, *HIDDEN_UNITS, 1])
+        mlp.compile(automatic_differentiation=False, learning_rate=0.01, run_eagerly=RUN_EAGERLY)
 
-    print(np.mean(y_test == y_test_pred, axis=0))
-    pass
+        hist = mlp.fit(u_train, y_train, batch_size=1, epochs=3, validation_split=0.2)
+        histories.append(hist)
+        y_test_pred = mlp.predict_class(u_test)
+
+        y_test_s.append(y_test)
+        y_test_pred_s.append(y_test_pred)
+        accuraccy.append(np.mean(y_test == y_test_pred, axis=0))
+
+    loss = np.array([x.history["loss"] for x in histories])
+    val_loss = np.array([x.history["val_loss"] for x in histories])
+
+    print(np.mean(accuraccy))
+    print(np.std(accuraccy))
 
 
 if __name__ == "__main__":